9869700fa47eadafecc1f248b21556c8677ca680
Gligor Kotushevski <gligorkot@gmail.com>
2017-08-03 15:25:39 +1200

    Delegate and bubble shake (#11)
    * changed digitsRemaining to an enteredPin String so we can keep track of what was entered so far
    
    * adding delegate methods to check if entered pin is valid using a block, the is done async so that the user can do a network request if needed, also show loading indicator while we’re waiting. updated example app to show how to use the new delegate methods
    
    * adding some documentation around the public properties and public protocol declarations
    
    * added a pin bubble shake animation on incorrect pin, as well as a delegate method that gets called when an incorrect pin has been entered

--- Classes/PinView.swift
+++ Classes/PinView.swift
@@ -9,7 +9,41 @@
 import UIKit
 
 public protocol PinViewDelegate: class {
+
+    /**
+     Function gets called when the user taps the `Cancel` button on the PinView
+     
+     - Parameters:
+        - pinView: The `PinView` that called the delegate function
+     */
     func pinViewDidTapCancel(pinView: PinView)
+    
+    /**
+     Function gets called when the user ends entering the 4 digit pin
+     
+     - Parameters:
+        - pinView: The `PinView` that called the delegate function
+        - enteredPin: The 4 digit pin the user has entered as a `String`
+        - isCorrectPinBlock: An escaping closure/block that takes an `isCorrect` boolean parameter
+        - isCorrect: The boolean that tells the `PinView` whether or not the entered pin is correct/valid
+     */
+    func pinView(pinView: PinView, enteredPin: String, isCorrectPinBlock: @escaping (_ isCorrect: Bool) -> ())
+    
+    /**
+     Function gets called when the user ends entering the 4 digit pin and the pin has been identified as a correct/valid pin
+     
+     - Parameters:
+        - pinView: The `PinView` that called the delegate function
+     */
+    func pinViewDidSucceed(pinView: PinView)
+    
+    /**
+     Function gets called when the user ends entering the 4 digit pin and the pin has been identified as an incorrect/invalid pin
+     
+     - Parameters:
+        - pinView: The `PinView` that called the delegate function
+     */
+    func pinViewDidFailWithIncorrectPin(pinView: PinView)
 }
 
 @IBDesignable
@@ -19,24 +53,34 @@ public final class PinView: UIView {
     @IBOutlet weak var rootView: UIView!
     @IBOutlet weak var visualEffectBackground: UIVisualEffectView!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
     @IBOutlet weak var pinBubbleOne: PinBubble!
     @IBOutlet weak var pinBubbleTwo: PinBubble!
     @IBOutlet weak var pinBubbleThree: PinBubble!
     @IBOutlet weak var pinBubbleFour: PinBubble!
     
     // MARK: - IBInspectables
+    /**
+     The title showing at the top of the `PinView`
+     */
     @IBInspectable public var title: String = "Enter Passcode" {
         didSet {
             titleLabel.text = title
         }
     }
     
+    /**
+     The boolean value specifying whether or not the `PinView` will have a blurred background or not
+    */
     @IBInspectable public var blurBackground: Bool = false {
         didSet {
             visualEffectBackground.isHidden = !blurBackground
         }
     }
     
+    /**
+     The color that controls the color of the `PinView` elements
+     */
     @IBInspectable public override var tintColor: UIColor! {
         didSet {
             updateViewsTintColor()
@@ -44,6 +88,9 @@ public final class PinView: UIView {
     }
     
     // MARK: - Other properties
+    /**
+     The blur background effect style that will be applied, if and only if `blurBackground` property is set to `true`
+    */
     public var blurBackgroundEffectStyle: UIBlurEffectStyle = .dark {
         didSet {
             visualEffectBackground.effect = UIBlurEffect(style: blurBackgroundEffectStyle)
@@ -51,7 +98,28 @@ public final class PinView: UIView {
     }
     
     // MARK: - State
-    fileprivate var digitsRemaining = 4
+    fileprivate var enteredPin = "" {
+        didSet {
+            if let delegate = delegate, enteredPin.length == 4 {
+                showLoading()
+                delegate.pinView(pinView: self, enteredPin: enteredPin, isCorrectPinBlock: { (isCorrect: Bool) in
+                    // force execute block on the main thread (as these functions change the UI), in case the user didn't do this
+                    DispatchQueue.main.async {
+                        if isCorrect {
+                            delegate.pinViewDidSucceed(pinView: self)
+                        } else {
+                            self.enteredPin = ""
+                            self.updateBubbles()
+                            self.shakeBubbles()
+                            delegate.pinViewDidFailWithIncorrectPin(pinView: self)
+                            self.hideLoading()
+                        }
+                    }
+                })
+            }
+            updateBubbles()
+        }
+    }
     
     // MARK: - Delegate
     public weak var delegate: PinViewDelegate?
@@ -105,27 +173,25 @@ private extension PinView {
     }
     
     @IBAction func backspaceTapped(_ sender: UIButton) {
-        if digitsRemaining < 4 {
-            digitsRemaining += 1
-            updateBubbles()
+        if enteredPin.length > 0 {
+            enteredPin.removeLastCharacter()
         }
     }
     
     @IBAction func keypadTapped(_ sender: PinButton) {
-        if digitsRemaining > 0 {
-            digitsRemaining -= 1
-            updateBubbles()
+        if enteredPin.length < 4 {
+            enteredPin = "\(enteredPin)\(sender.digit)"
         }
     }
     
     func updateBubbles() {
-        switch digitsRemaining {
-        case 4:
+        switch enteredPin.length {
+        case 0:
             pinBubbleOne.isFilled = false
             pinBubbleTwo.isFilled = false
             pinBubbleThree.isFilled = false
             pinBubbleFour.isFilled = false
-        case 3:
+        case 1:
             pinBubbleOne.isFilled = true
             pinBubbleTwo.isFilled = false
             pinBubbleThree.isFilled = false
@@ -135,12 +201,12 @@ private extension PinView {
             pinBubbleTwo.isFilled = true
             pinBubbleThree.isFilled = false
             pinBubbleFour.isFilled = false
-        case 1:
+        case 3:
             pinBubbleOne.isFilled = true
             pinBubbleTwo.isFilled = true
             pinBubbleThree.isFilled = true
             pinBubbleFour.isFilled = false
-        case 0:
+        case 4:
             pinBubbleOne.isFilled = true
             pinBubbleTwo.isFilled = true
             pinBubbleThree.isFilled = true
@@ -152,6 +218,35 @@ private extension PinView {
         pinBubbleTwo.setNeedsDisplay()
         pinBubbleThree.setNeedsDisplay()
         pinBubbleFour.setNeedsDisplay()
+    }
+    
+    func showLoading() {
+        titleLabel.isHidden = true
+        loadingIndicator.isHidden = false
+    }
+    
+    func hideLoading() {
+        loadingIndicator.isHidden = true
+        titleLabel.isHidden = false
+    }
+    
+    func shakeBubbles() {
+        pinBubbleOne.shake()
+        pinBubbleTwo.shake()
+        pinBubbleThree.shake()
+        pinBubbleFour.shake()
+    }
+    
+}
+
+private extension String {
+    
+    var length: Int {
+        return self.characters.count
+    }
+    
+    mutating func removeLastCharacter() {
+        self.remove(at: self.index(before: self.endIndex))
     }
     
 }
--- GKPinViewExample/GKPinViewExample/PinViewController.swift
+++ GKPinViewExample/GKPinViewExample/PinViewController.swift
@@ -33,4 +33,23 @@ extension PinViewController: PinViewDelegate {
         dismiss(animated: true, completion: nil)
     }
     
+    func pinView(pinView: PinView, enteredPin: String, isCorrectPinBlock: @escaping (_ isCorrect: Bool) -> ()) {
+        DispatchQueue.global(qos: .background).async {
+            // do a network request to check pin, or just check locally async in the background
+            sleep(2)
+            DispatchQueue.main.async {
+                // execute isValidPinBlock on the main thread
+                isCorrectPinBlock(enteredPin == "1234")
+            }
+        }
+    }
+    
+    func pinViewDidSucceed(pinView: PinView) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func pinViewDidFailWithIncorrectPin(pinView: PinView) {
+        pinView.title = "Incorrect passcode, please try again"
+    }
+    
 }
